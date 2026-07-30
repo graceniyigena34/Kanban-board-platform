0e464a978e0ba1bb7a7cb28b60361306e6235a53
graceniyigena34 <graceniyigena34@gmail.com>
2026-07-30 11:51:11 +0200

    server running successful

---
+++ kanban-bn/src/app.ts
@@ -0,0 +1,24 @@
+import express from "express";
+import cors from "cors";
+
+import authRoutes from "./routes/auth.routes";
+
+
+const app = express();
+
+
+app.use(cors());
+app.use(express.json());
+
+
+app.use("/api/auth", authRoutes);
+
+
+app.get("/",(req,res)=>{
+ res.json({
+  message:"TaskFlow API running successfully"
+ });
+});
+
+
+export default app;
---
+++ kanban-bn/src/services/auth.service.ts
@@ -0,0 +1,89 @@
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
+import prisma from "../config/db";
+
+
+export const registerUser = async (
+  name: string,
+  email: string,
+  password: string
+) => {
+
+  const existingUser = await prisma.user.findUnique({
+    where: {
+      email
+    }
+  });
+
+
+  if (existingUser) {
+    throw new Error("User already exists");
+  }
+
+
+  const hashedPassword = await bcrypt.hash(password, 10);
+
+
+  const user = await prisma.user.create({
+    data:{
+      name,
+      email,
+      password: hashedPassword
+    }
+  });
+
+
+  return user;
+};
+
+
+
+export const loginUser = async (
+  email:string,
+  password:string
+)=>{
+
+  const user = await prisma.user.findUnique({
+    where:{
+      email
+    }
+  });
+
+
+  if(!user){
+    throw new Error("Invalid email or password");
+  }
+
+
+  const passwordMatch = await bcrypt.compare(
+    password,
+    user.password
+  );
+
+
+  if(!passwordMatch){
+    throw new Error("Invalid email or password");
+  }
+
+
+  const token = jwt.sign(
+    {
+      userId:user.id
+    },
+    process.env.JWT_SECRET as string,
+    {
+      expiresIn:"1d"
+    }
+  );
+
+
+  return {
+    user:{
+      id:user.id,
+      name:user.name,
+      email:user.email
+    },
+    token
+  };
+
+};
